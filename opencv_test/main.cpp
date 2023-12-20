cf3ab7f74ae26f55e099828370705ead83ff6b72
minwoo <gomer72kd@gmail.com>
2023-12-21 03:21:33 +0900

    Initial Commit

---
+++ opencv_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opencv_test
+//
+//  Created by 정민우 on 12/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
